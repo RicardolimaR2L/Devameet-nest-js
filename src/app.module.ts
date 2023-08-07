6d728c79e2db04e5341b8fe268556dd78d04728f
Ricardo <ricardolimadelemos.trab@gmail.com>
2023-08-07 12:45:52 -0300

    Jwt  Guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD} from '@nestjs/core'
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { JwtAuthGuard } from './auth/guards/jwt.guards';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { UserModule } from './user/user.module';
     UserModule
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {provide: APP_GUARD, useClass: JwtAuthGuard} //traz pra todo o sistema o Guard  fazendo todo ele estar protegido.
+  ],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller,Post, HttpCode, HttpStatus, Body} from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dtos/login.dto";
 import { RegisterDto } from "src/user/dtos/register.dto";
+import { isPublic } from "./decorators/ispublic.decorator";
+
 
 @Controller("auth")
 export class AuthController{
@@ -9,12 +11,14 @@ export class AuthController{
 
       @Post('login')
       @HttpCode(HttpStatus.OK)
+      @isPublic()
       login(@Body() dto: LoginDto){
         return this.AuthService.login(dto);
-
+        
       }
       @Post('register')
       @HttpCode(HttpStatus.OK)
+      @isPublic()
       register(@Body() dto: RegisterDto){
         return this.AuthService.register(dto);
 
