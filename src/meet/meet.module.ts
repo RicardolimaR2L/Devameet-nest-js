6e134227e938da192658b695fc5da325d1914ba7
Ricardo <ricardolimadelemos.trab@gmail.com>
2023-08-11 23:19:47 -0300

    Execução de salas Pt. 3

--- src/meet/meet.module.ts
+++ src/meet/meet.module.ts
@@ -4,22 +4,19 @@ import { MeetService } from './meet.service';
 import { UserModule } from 'src/user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Meet, MeetSchema } from './schemas/meet.schema';
-import { MeetObject, MeetObjectSchema} from './schemas/meetobjects.schema';
+import { MeetObject, MeetObjectSchema } from './schemas/meetobjects.schema';
 import { RoomService } from 'src/room/room.service';
-
 
 @Module({
   imports: [
     UserModule,
     MongooseModule.forFeature([
       { name: Meet.name, schema: MeetSchema },
-      { name: MeetObject.name, schema: MeetObjectSchema }
-    
+      { name: MeetObject.name, schema: MeetObjectSchema },
     ]),
   ],
   controllers: [MeetController],
-  providers: [MeetService, RoomService],
-  exports: [MongooseModule, MeetService, 
-  ],
+  providers: [MeetService],
+  exports: [MongooseModule, MeetService],
 })
 export class MeetModule {}
--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -10,24 +10,27 @@ import { Position, PositionDocument } from './schemas/position.schema';
 import { UserService } from 'src/user/user.service';
 import { RoomMessagesHelper } from './helpers/roommessage.helper';
 import { UpdateUserPostionDto } from './dtos/updateposition.dto';
+import { ToglMuteDto } from './dtos/toglMute.Dto';
 
 @Injectable()
 export class RoomService {
   private logger = new Logger(RoomService.name);
 
-   constructor(
-        @InjectModel(Meet.name) private readonly meetModel: Model<MeetDocument>,
-        @InjectModel(MeetObject.name) private readonly objectModel: Model<MeetObjectDocument>,
-        @InjectModel(Position.name) private readonly positionModel: Model<PositionDocument>,
-        private readonly userService: UserService
-    ) { }
+  constructor(
+    @InjectModel(Meet.name) private readonly meetModel: Model<MeetDocument>,
+    @InjectModel(MeetObject.name)
+    private readonly objectModel: Model<MeetObjectDocument>,
+    @InjectModel(Position.name)
+    private readonly positionModel: Model<PositionDocument>,
+    private readonly userService: UserService,
+  ) {}
 
-  async getRoom(link: string) {//entra na sala pelo link
+  async getRoom(link: string) {
     this.logger.debug(`getRoom -${link}`);
 
     const meet = await this._getMeet(link);
 
-    const objects = this.objectModel.find({ meet }); // retornamos os objects da sala 
+    const objects = await this.objectModel.find({ meet });
     return {
       link,
       name: meet.name,
@@ -41,13 +44,55 @@ export class RoomService {
     const meet = await this._getMeet(link);
     return await this.positionModel.find({ meet });
   }
-  async deleteUsersPosition(clientId: String) { // deletar a posição dos ususarios pelo clientID
+  async deleteUsersPosition(clientId: String) {
     this.logger.debug(`deleteUsersPosition -${clientId}`);
     return await this.positionModel.deleteMany({ clientId });
   }
 
-  updateUserPosition(dto: UpdateUserPostionDto){
+  async updateUserPosition(clientId: string, dto: UpdateUserPostionDto) {
+    this.logger.debug(` updateUsersPosition - ${dto.link}`);
+    const meet = await this._getMeet(dto.link);
+    const user = await this.userService.getUserById(dto.userId);
 
+    if (!user) {
+      throw new BadRequestException(RoomMessagesHelper.JOIN_LINK_NOT_VALID);
+    }
+    const position = {
+      ...dto,
+      clientId,
+      user,
+      meet,
+      name: user.name,
+      avatar: user.avatar,
+    };
+
+    const usersInRoom = await this.positionModel.find({ meet });
+
+    const loggedUserInRoom = usersInRoom.find(
+      (u) =>
+        u.user.toString() === user._id.toString() || u.clientId === clientId,
+    );
+
+    if (loggedUserInRoom) {
+      await this.positionModel.findByIdAndUpdate(
+        { _id: loggedUserInRoom._id },
+        position,
+      );
+    } else {
+      if (usersInRoom && usersInRoom.length > 10) {
+        throw new BadRequestException(RoomMessagesHelper.ROOM_MAX_USERS);
+      }
+
+      await this.positionModel.create(position);
+    }
+  }
+
+  async updateUserMute(dto: ToglMuteDto) {
+    this.logger.debug(` updateUserMute -${dto.link} - ${dto.userId}`);
+
+    const meet = await this._getMeet(dto.link);
+    const user = await this.userService.getUserById(dto.userId);
+    await this.positionModel.updateMany({ user, meet }), { muted: dto.muted };
   }
 
   async _getMeet(link: string) {
