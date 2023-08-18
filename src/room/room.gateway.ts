975e186fe7d4d4f6b28b30366ff88be1fa2f36d2
Ricardo <ricardolimadelemos.trab@gmail.com>
2023-08-17 23:37:31 -0300

    Atualizar Room.Gateway

--- src/room/room.gateway.ts
+++ src/room/room.gateway.ts
@@ -1,89 +1,101 @@
-import {
-  SubscribeMessage,
-  WebSocketGateway,
-  OnGatewayInit,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
-  WebSocketServer,
-} from '@nestjs/websockets';
+ import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { RoomService } from './room.service';
 import { Logger } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
 import { JoinRoomDto } from './dtos/joinroom.dto';
-import { UpdateUserPostionDto } from './dtos/updateposition.dto';
-import { ToglMuteDto } from './dtos/toglMute.Dto';
+import {  UpdateUserPostionDto } from './dtos/updateposition.dto';
+import { ToglMuteDto } from './dtos/toglMute.dto';
 
 type ActiveSocketType = {
-  room: string;
+  room: String;
   id: string;
   userId: string;
-};
+}
 
 @WebSocketGateway({ cors: true })
 export class RoomGateway implements OnGatewayInit, OnGatewayDisconnect {
-  wws: any;
-  constructor(private readonly service: RoomService) {}
+
+  constructor(private readonly service: RoomService) { }
+
   @WebSocketServer() wss: Server;
 
   private logger = new Logger(RoomGateway.name);
   private activeSockets: ActiveSocketType[] = [];
 
-  handleDisconnect(client: any, ...args: any[]) {
-    this.logger.debug(`client: ${client.id} disconnected`);
+  async handleDisconnect(client: any) {
+    const existingOnSocket = this.activeSockets.find(
+      socket => socket.id === client.id
+    );
+
+    if (!existingOnSocket) return;
+
+    this.activeSockets = this.activeSockets.filter(
+      socket => socket.id !== client.id
+    );
+
+    await this.service.deleteUsersPosition(client.id);
+    client.broadcast.emit(`${existingOnSocket.room}-remove-user`, { socketId: client.id });
+
+    this.logger.debug(`Client: ${client.id} disconnected`);
   }
-  afterInit(client: any, ...args: any[]) {
+
+  afterInit(server: any) {
     this.logger.log('Gateway initialized');
   }
+
   @SubscribeMessage('join')
   async handleJoin(client: Socket, payload: JoinRoomDto) {
     const { link, userId } = payload;
 
     const existingOnSocket = this.activeSockets.find(
-      (socket) => socket.room === link && socket.id === client.id,
-    );
+      socket => socket.room === link && socket.id === client.id);
 
     if (!existingOnSocket) {
       this.activeSockets.push({ room: link, id: client.id, userId });
+
       const dto = {
         link,
         userId,
-        x: 2,
-        y: 2,
-        orientation: 'down',
-      } as UpdateUserPostionDto;
+        x: 1,
+        y: 1,
+        orientation: 'front'
+      } as UpdateUserPostionDto
 
       await this.service.updateUserPosition(client.id, dto);
-      const users = await this.service.listenUsersPositionByLink(link);
 
-      this.wws.emit(`${link} -update-user-list`, { users });
-      client.broadcast.emit(`${link} -add-user-list,`, { user: client.id });
     }
 
-    return this.logger.debug(
-      ` Socket client: ${client.id} starr to join room ${link}`,
-    );
+    const users = await this.service.listenUsersPositionByLink(link);
+    this.wss.emit(`${link}-update-user-list`, { users });
+
+    if (!existingOnSocket) {
+      client.broadcast.emit(`${link}-add-user`, { user: client.id });
+    }
+
+    this.logger.debug(`Socket client: ${client.id} start to join room ${link}`);
   }
+
   @SubscribeMessage('move')
   async handleMove(client: Socket, payload: UpdateUserPostionDto) {
-    const { link, userId, x, y, orientation } = payload;
+    const { link, userId, x, y,orientation } = payload;
     const dto = {
       link,
       userId,
       x,
       y,
-      orientation: 'down',
-    } as UpdateUserPostionDto;
+      orientation
+    } as UpdateUserPostionDto
+
     await this.service.updateUserPosition(client.id, dto);
     const users = await this.service.listenUsersPositionByLink(link);
-    this.wws.emit(`${link} -update-user-list`, { users });
+    this.wss.emit(`${link}-update-user-list`, { users });
   }
 
-  @SubscribeMessage('Toggl-Mute-User')
+  @SubscribeMessage('toggl-mute-user')
   async handleToglMute(_: Socket, payload: ToglMuteDto) {
     const { link } = payload;
-
     await this.service.updateUserMute(payload);
     const users = await this.service.listenUsersPositionByLink(link);
-    this.wws.emit(`${link} -update-user-list`, { users });
+    this.wss.emit(`${link}-update-user-list`, { users });
   }
 }
